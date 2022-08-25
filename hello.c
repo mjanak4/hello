bfa5962d076c42b4d20260e07ff65a4bfd81e94a
mjanak4 <mjanak4@huskers.unl.edu>
2022-08-25 01:30:02 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Michael Janak
+ *         mjanak4@huskers.unl.edu
+ * Date: 2022/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Michael Janak\n");
+
+  return 0;
+}
